a9392c84d2959414b9a8b3a3518f3f49cf534f6e
SanketPathare <sanketpathare8808@gmail.com>
2024-12-31 01:02:42 +0530

    15-commit-error-fix

--- Components/BlogItem.tsx
+++ Components/BlogItem.tsx
@@ -1,41 +1,48 @@
-
-
 import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa"
+import { FaArrowRight } from "react-icons/fa";
 
 interface BlogItemProps {
-    title: string;
-    description: string;
-    image: string; 
-    category: string;
-    id: string;
-  }
-  
+  title: string;
+  description: string;
+  image: string;
+  category: string;
+  id: string;
+}
 
-  const BlogItem: React.FC<BlogItemProps> = ({ title, description, category, image, id }) => {
-
+const BlogItem: React.FC<BlogItemProps> = ({
+  title,
+  description,
+  category,
+  image,
+  id,
+}) => {
   return (
     <div className="max-w-[330px] sm:max-[300px] bg-[#000] hover:bg-[#ffffff05] border-2 hover:border-gray-600 border-gray-500/40   p-4 rounded-lg">
-     <Link href={`/blogs/${id}`}>
-     
-      {/* <Image src={image} alt="" width={400} height={400} className="border-b border-black " /> */}
-      <img src={image} alt="" className="w-[400px] h-[300px] rounded-lg "  />
-     </Link>
+      <Link href={`/blogs/${id}`}>
+        {/* <Image src={image} alt="" width={400} height={400} className="border-b border-black " /> */}
+        <img src={image} alt="" className="w-[400px] h-[300px] rounded-lg " />
+      </Link>
       <p className="ml-5  mt-5 px-1 inline-block bg-white  text-black text-sm rounded-sm ">
         {category}
       </p>
-       <div className="p-5">
-         <h5 className="mb-2 text-lg font-medium tracking-tight text-white">
+      <div className="p-5">
+        <h5 className="mb-2 text-lg font-medium tracking-tight text-white">
           {title}
-         </h5>
-         <p className="mb-3 text-sm tracking-tight text-gray-300/70" dangerouslySetInnerHTML={{ __html: description.slice(0, 120) }}>
-         </p>
-       <Link href={`/blogs/${id}`} className="inline-flex items-center py-2 font-bold text-center  gap-2 text-white ">
-        Read more<FaArrowRight  />
-       </Link>
-       </div>
+        </h5>
+        <p
+          className="mb-3 text-sm tracking-tight text-gray-300/70"
+          dangerouslySetInnerHTML={{ __html: description.slice(0, 120) }}
+        ></p>
+        <Link
+          href={`/blogs/${id}`}
+          className="inline-flex items-center py-2 font-bold text-center  gap-2 text-white "
+        >
+          Read more
+          <FaArrowRight />
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogItem
+export default BlogItem;
--- Components/BlogList.tsx
+++ Components/BlogList.tsx
@@ -1,65 +1,33 @@
+
 import BlogItem from "./BlogItem";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+interface BlogData {
+  _id: string;
+  image: string;
+  title: string;
+  description: string;
+  category: string;
+}
+
 const BlogList = () => {
   const [menu] = useState("All");
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState<BlogData[]>([]);
 
   const fetchBlogs = async () => {
     const response = await axios.get("/api/blog");
     setBlogs(response.data.blogs);
     console.log(response.data.blogs);
   };
+
   useEffect(() => {
     fetchBlogs();
   }, []);
 
   return (
-    <div className="mb-10">
-      {/* <div className="flex justify-center gap-6 my-10">
-        <button
-          onClick={() => SetMenu("All")}
-          className={
-            menu === "All"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : "bg-gray-200 text-gray-600 py-1 px-4 rounded-sm "
-          }
-        >
-          All
-        </button>
-        <button
-          onClick={() => SetMenu("Technology")}
-          className={
-            menu === "Technology"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : "bg-gray-200 text-gray-600 py-1 px-4 rounded-sm "
-          }
-        >
-          Tech
-        </button>
-        <button
-          onClick={() => SetMenu("Startup")}
-          className={
-            menu === "Startup"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : "bg-gray-200 text-gray-600 py-1 px-4 rounded-sm "
-          }
-        >
-          Startup
-        </button>
-        <button
-          onClick={() => SetMenu("Lifestyle")}
-          className={
-            menu === "Lifestyle"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : "bg-gray-200 text-gray-600 py-1 px-4 rounded-sm "
-          }
-        >
-          Lifestyle
-        </button>
-      </div> */}
-      <div className="flex  flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+    <div>
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
         {blogs
           .filter((item) => {
             return menu === "All" ? true : item.category === menu;
