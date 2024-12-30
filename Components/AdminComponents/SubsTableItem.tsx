813c351f7f949ef8792623fc9117de2e323d03c8
SanketPathare <sanketpathare8808@gmail.com>
2024-12-31 00:15:39 +0530

    13-commit-error-fix

--- Components/AdminComponents/SubsTableItem.tsx
+++ Components/AdminComponents/SubsTableItem.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { RiDeleteBin6Line } from 'react-icons/ri';
+import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
-const SubsTableItem = ({email,mongoId,deleteEmail,date}) => {
-    
-    const emailDate = new Date(date);
+const SubsTableItem: React.FC<{
+  email: string;
+  mongoId: string;
+  deleteEmail: (id: string) => void;
+  date: string;
+}> = ({ email, mongoId, deleteEmail, date }) => {
+  const emailDate = new Date(date);
 
   return (
-    <tr className='bg-white border-b text-left'>
-      <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
-        {email?email:"No Email"}
+    <tr className="bg-white border-b text-left">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+      >
+        {email ? email : "No Email"}
       </th>
-      <td className='px-6 py-4'>{emailDate.toDateString()}</td>
-      <td  className=" text-lg text-black px-6 py-4 cursor-pointer" onClick={()=>deleteEmail(mongoId)}>
-      <RiDeleteBin6Line />
+      <td className="px-6 py-4">{emailDate.toDateString()}</td>
+      <td
+        className=" text-lg text-black px-6 py-4 cursor-pointer"
+        onClick={() => deleteEmail(mongoId)}
+      >
+        <RiDeleteBin6Line />
       </td>
     </tr>
-  )
-}
+  );
+};
 
-export default SubsTableItem
+export default SubsTableItem;
